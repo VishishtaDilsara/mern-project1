f17a5ad87d930807fc6ad175717357ae0e79a914
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-05-02 22:26:02 +0530

    get request created for req and res

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 import express from "express";
 
 let app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+    message2: "Hello World 2",
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is running in port 3000");
 });
